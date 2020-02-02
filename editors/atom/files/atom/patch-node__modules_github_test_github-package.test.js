d6f22719c59f7d2af5830db20d5786efb51ac643
Hiroki Tagato <tagattie@gmail.com>
2020-02-02 19:10:30 +0900

    Update to v1.43.0.

--- editors/atom/files/atom/patch-node__modules_github_test_github-package.test.js
+++ editors/atom/files/atom/patch-node__modules_github_test_github-package.test.js
@@ -1,20 +1,20 @@
---- node_modules/github/test/github-package.test.js.orig	2019-12-27 06:39:15 UTC
+--- node_modules/github/test/github-package.test.js.orig	2019-12-11 02:42:35 UTC
 +++ node_modules/github/test/github-package.test.js
-@@ -644,7 +644,7 @@ describe('GithubPackage', function() {
-     });
+@@ -935,7 +935,7 @@ describe('GithubPackage', function() {
  
-     it('refreshes the appropriate Repository and Atom GitRepository when a file is changed in workspace 1', async function() {
--      if (process.platform === 'linux') {
-+      if (process.platform === 'linux' || process.platform === 'freebsd') {
-         this.skip();
-       }
-       this.retries(5); // FLAKE
-@@ -656,7 +656,7 @@ describe('GithubPackage', function() {
-     });
+     describe('when a file change is made outside Atom in workspace 1', function() {
+       beforeEach(function() {
+-        if (process.platform === 'linux') {
++        if (process.platform === 'linux' || process.platform === 'freebsd') {
+           this.skip();
+         }
  
-     it('refreshes the appropriate Repository and Atom GitRepository when a file is changed in workspace 2', async function() {
--      if (process.platform === 'linux') {
-+      if (process.platform === 'linux' || process.platform === 'freebsd') {
-         this.skip();
-       }
-       this.retries(5); // FLAKE
+@@ -953,7 +953,7 @@ describe('GithubPackage', function() {
+ 
+     describe('when a file change is made outside Atom in workspace 2', function() {
+       beforeEach(function() {
+-        if (process.platform === 'linux') {
++        if (process.platform === 'linux' || process.platform === 'freebsd') {
+           this.skip();
+         }
+ 
